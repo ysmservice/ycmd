0e64cd578d3f9dabe39fd8a92ab4f5351c6972da
ysmreg <33682244+ysmreg@users.noreply.github.com>
2022-04-29 21:03:50 +0900

    Update __init__.py

--- python/ycmd/__init__.py
+++ python/ycmd/__init__.py
@@ -1,1 +1,11 @@
+import sys
+from .ycmd import CMD
 
+def main():
+  cmd = CMD()
+  if len(sys.argv) == 2:
+    cmd.cmdrun_file(sys.argv[1])
+  if len(sys.argv) == 2:
+    cmd.cmdrun_method_file(sys.argv[1],sys.argv[2])
+  if len(sys.argv) == 1:
+    print("ファイルパスを指定してください")
