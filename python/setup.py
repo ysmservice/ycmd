85743cb0488ff8946d04d1f997b1cffbde8233f4
ysmreg <33682244+ysmreg@users.noreply.github.com>
2022-06-06 18:46:21 +0900

    Add files via upload

--- python/setup.py
+++ python/setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r",encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="ysmreg",
-    version="0.0.1",
+    name="ycmd",
+    version="0.0.2",
     install_requires=[
         
     ],
